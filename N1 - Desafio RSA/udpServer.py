529a0ba415bc96cd1860e92a2a14aea4185c71db
YsabelaMolero <ysabelamoleror@gmail.com>
2023-09-01 20:16:09 -0300

    Fix: Ajustes no codigo para testes

--- N1 - Desafio RSA/udpServer.py
+++ N1 - Desafio RSA/udpServer.py
@@ -1,25 +1,28 @@
-import socket
-import random
-import math
+from socket import *
+from random import *
+from math import *
 from rsa import RSA
 
+rsa = RSA()
+
+
+serverPort = 12500
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(("",serverPort))
+print ("UDP server\n")
+    
 def main():
     bit_size = 4096
     _, private_key = generate_keypair(bit_size)
-
-    serverPort = 12500
-    serverSocket = socket(AF_INET, SOCK_DGRAM)
-    serverSocket.bind(("",serverPort))
-    print ("UDP server\n")
-
+    
     while True:
         encrypted_data, client_address = server_socket.recvfrom(4096)
         encrypted_message = list(map(int, encrypted_data.decode('utf-8').split(',')))
 
-        decrypted_message = rsa.decrypt(private_key, encrypted_message)
-        rsa = RSA()
+        decrypted_message = RSA.decrypt(private_key, encrypted_message)
+        
         print("Mensagem Recebida:", decrypted_message)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
